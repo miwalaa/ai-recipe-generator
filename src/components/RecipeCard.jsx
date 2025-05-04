3237260d9abdb95c7f7d94d82caf3cb8471e074a
miwalaa <anonimdante79@gmail.com>
2025-05-04 22:15:35 +0700

    Initial change

--- src/components/RecipeCard.jsx
+++ src/components/RecipeCard.jsx
@@ -6,7 +6,7 @@ export default function RecipeResult({ recipe }) {
     <>
       <img src={sandwich} alt="sandwich icon" className='w-35 mb-2' />
       <h2 className="text-4xl md:text-5xl font-bold px-2 mb-3.5 text-center">Generated Recipe</h2>
-      <div className='result-recipe-container prose prose-sm sm:prose-base max-w-none text-gray-800 border-2 rounded-md' aria-live='polite'>
+      <div className='result-recipe-container prose prose-sm sm:prose-base max-w-none text-gray-800 border-2 rounded-md shadow-md' aria-live='polite'>
         <ReactMarkdown>{recipe}</ReactMarkdown>
       </div>
     </>
